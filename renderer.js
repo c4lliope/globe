c02d5a737ca054f5e767f933c39a7d63088e0a66
grace <grace.c.youngblood@gmail.com>
2020-11-14 20:52:51 -0500

    drop placemark

--- renderer.js
+++ renderer.js
@@ -8,32 +8,6 @@ wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-// Add a placemark
-var placemarkLayer = new WorldWind.RenderableLayer();
-wwd.addLayer(placemarkLayer);
-
-var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-
-placemarkAttributes.imageOffset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.3,
-    WorldWind.OFFSET_FRACTION, 0.0);
-
-placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.5,
-    WorldWind.OFFSET_FRACTION, 1.0);
-
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-
-var position = new WorldWind.Position(55.0, -106.0, 100.0);
-var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
-
-placemark.label = "Placemark\n" +
-    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-    "Lon " + placemark.position.longitude.toPrecision(5).toString();
-placemark.alwaysOnTop = true;
-
-placemarkLayer.addRenderable(placemark);
-
 // Add a polygon
 var polygonLayer = new WorldWind.RenderableLayer();
 wwd.addLayer(polygonLayer);
