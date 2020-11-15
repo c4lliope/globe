89fe3ec243afaa350b4e5bfd1276fec34ff568dd
grace <grace.c.youngblood@gmail.com>
2020-11-14 21:35:54 -0500

    display kalamazoo and ann arbor; drop bermuda triangle.

--- renderer.js
+++ renderer.js
@@ -8,24 +8,43 @@ wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-// Add a polygon
+// Add Michigan counties
 var polygonLayer = new WorldWind.RenderableLayer();
 wwd.addLayer(polygonLayer);
 
-var polygonAttributes = new WorldWind.ShapeAttributes(null);
-polygonAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.75);
-polygonAttributes.outlineColor = WorldWind.Color.BLUE;
-polygonAttributes.drawOutline = true;
-polygonAttributes.applyLighting = true;
-
-var boundaries = [];
-boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
-boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
-boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
-
-var polygon = new WorldWind.Polygon(boundaries, polygonAttributes);
-polygon.extrude = true;
-polygonLayer.addRenderable(polygon);
+var json = {
+    type: "FeatureCollection",
+    properties: {
+        kind: "state",
+        state: "MI"
+    },
+    features:[{
+        type: "Feature",
+        properties: {
+            kind: "county",
+            name: "Kalamazoo",
+            state: "MI"
+        },
+        geometry:{
+            type:"MultiPolygon",
+            coordinates: [[[
+                [-85.5410,42.4224],
+                [-85.3000,42.4170],
+                [-85.2945,42.0719],
+                [-85.3000,42.0719],
+                [-85.7655,42.0719],
+                [-85.7655,42.4224]
+            ]]]
+        }
+    }]
+}
+var kalamazoo = new WorldWind.GeoJSONParser(json)
+kalamazoo.addRenderablesForFeatureCollection(polygonLayer, json)
+var annArborJSON = {"type":"FeatureCollection","properties":{"kind":"state","state":"MI"},"features":[
+    {"type":"Feature","properties":{"kind":"county","name":"Washtenaw","state":"MI"},"geometry":{"type":"MultiPolygon","coordinates":[[[[-83.5528,42.4334],[-83.5419,42.0829],[-83.7719,42.0829],[-84.1334,42.0719],[-84.1334,42.4224],[-83.6624,42.4334]]]]}}
+    ]}
+var annArbor = new WorldWind.GeoJSONParser(annArborJSON)
+annArbor.addRenderablesForFeatureCollection(polygonLayer, annArborJSON)
 
 // Add WMS imagery
 var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
